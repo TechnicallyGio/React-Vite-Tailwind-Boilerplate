59e6e466ee6b8f22a329401d016da3d32f8533ea
Giovanni <gmedrano@aorux.com>
2023-12-30 17:36:28 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         </a>
       </div>
       <h1 className='text-center'>Vite + React + TailwindCSS</h1>
-      <p>Vite + React + TailwindCSS</p>
+      <p>Boilerpate</p>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
